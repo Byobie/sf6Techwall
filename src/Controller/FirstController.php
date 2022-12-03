117ad3f34adb6df07ac267c2fdf020136daebdd5
Byob <clumpf@protonmail.com>
2022-12-03 10:47:20 +0100

    Adding jeanne controller

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -11,8 +11,15 @@ class FirstController extends AbstractController
     #[Route('/first', name: 'app_first')]
     public function index(): Response
     {
-        return $this->render('first/index.html.twig', [
-            'controller_name' => 'FirstController',
-        ]);
+        return $this->render('first/index.html.twig');
+    }
+
+    #[Route('/sayJeanne', name: 'say_jeanne')]
+    public function sayJeanne(): Response
+    {
+        //return $this->redirectToRoute('app_first');
+        //return $this->forward('App\Controller\FirstController::index');
+        return $this->render('first/sayJeanne.html.twig', 
+        ['name' => 'JEANNE !', 'expression' => 'OSKOUR !']);
     }
 }
